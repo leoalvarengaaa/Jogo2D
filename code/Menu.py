79c403ff635e57c9736283afcd9ccd0a0466b2ef
User <alvarengaleoo@gmail.com>
2025-03-25 20:50:13 -0300

    Creating main loop Connecting Menu and Game classes

--- code/Menu.py
+++ code/Menu.py
@@ -21,8 +21,8 @@ class Menu:
         while True:
             # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Shooting", C_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Plane", C_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
